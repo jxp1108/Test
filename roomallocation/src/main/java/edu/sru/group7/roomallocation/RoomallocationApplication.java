c6dab41aa46938af31faa423bab1ac5515a57941
jxp1108 <jxp1108@sru.edu>
2024-01-28 01:59:33 -0500

    test upload

---
+++ roomallocation/src/main/java/edu/sru/group7/roomallocation/RoomallocationApplication.java
@@ -0,0 +1,13 @@
+package edu.sru.group7.roomallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomallocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomallocationApplication.class, args);
+	}
+
+}
---
+++ roomallocation/src/test/java/edu/sru/group7/roomallocation/RoomallocationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sru.group7.roomallocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomallocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
